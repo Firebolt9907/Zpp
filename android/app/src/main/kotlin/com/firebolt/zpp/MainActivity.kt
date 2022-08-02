593a8bbbfaee4c4e2cbe934e86a79285d410b07c
Rishu Sharma <rishushar99@gmail.com>
2022-08-02 15:52:45 +0530

    added splash screens (maybe)

--- android/app/src/main/kotlin/com/firebolt/zpp/MainActivity.kt
+++ android/app/src/main/kotlin/com/firebolt/zpp/MainActivity.kt
@@ -2,6 +2,23 @@ package com.firebolt.zpp
 
 import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
+// Somewhere in your app, before your FlutterFragment is needed,
+// like in the Application class ...
+// Instantiate a FlutterEngine.
+FlutterEngine flutterEngine = new FlutterEngine(context);
+
+// Start executing Dart code in the FlutterEngine.
+flutterEngine.getDartExecutor().executeDartEntrypoint(
+DartEntrypoint.createDefault()
+);
+
+// Cache the pre-warmed FlutterEngine to be used later by FlutterFragment.
+FlutterEngineCache
+.getInstance()
+.put("engine", flutterEngine);
+
+FlutterFragment.withCachedEngine("engine").build();
+
 
 class MainActivity: FlutterActivity() {
     override fun onPostResume() {
